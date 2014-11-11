fad85705ef404a05a2dc208e6f6204299e63d68d
StanDimitroff <standimitroff@gmail.com>
2014-11-12 00:53:14 +0200

    defensive programming hw added

--- 06. Unit-Testing-Homework/TestCustomLinkedList/DynamicListTest.cs
+++ 06. Unit-Testing-Homework/TestCustomLinkedList/DynamicListTest.cs
@@ -8,31 +8,35 @@
     [TestClass]
     public class DynamicListTest
     {
+        private DynamicList<char> dynamicList;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            this.dynamicList = new DynamicList<char>();
+        }
+
         [TestMethod]
         public void GettingListCountShouldReturnCorrectValue()
         {
-            var dynamicList = new DynamicList<char>();
-            dynamicList.Add('a');
-
-            Assert.AreEqual(1, dynamicList.Count, "Getter doesn't return correct list count.");
+            this.dynamicList.Add('a');
+            Assert.AreEqual(1, this.dynamicList.Count, "Getter doesn't return correct list count.");
         }
 
         [TestMethod]
         public void AddingItemShouldIncrementListCountByOne()
         {
-            var dynamicList = new DynamicList<char>();
-            dynamicList.Add('a');
-            dynamicList.Add('b');
+            this.dynamicList.Add('a');
+            this.dynamicList.Add('b');
 
-            Assert.AreEqual(2, dynamicList.Count, "Add method doesn't increment list count by one.");
+            Assert.AreEqual(2, this.dynamicList.Count, "Add method doesn't increment list count by one.");
         }
 
         [TestMethod]
         public void AddingItemShouldReturnCorrectAddedItem()
         {
-            var dynamicList = new DynamicList<char>();
-            dynamicList.Add('a');
-            var added = dynamicList[0];
+            this.dynamicList.Add('a');
+            var added = this.dynamicList[0];
 
             Assert.AreEqual('a', added, "Add method doesn't add the correct item.");
         }
@@ -40,13 +44,12 @@
         [TestMethod]
         public void RemovingValidItemShouldDecrementListCountByOne()
         {
-            var dynamicList = new DynamicList<char>();
-            dynamicList.Add('a');
-            dynamicList.Remove('a');
+            this.dynamicList.Add('a');
+            this.dynamicList.Remove('a');
 
             Assert.AreEqual(
                 0,
-                dynamicList.Count,
+                this.dynamicList.Count,
                 "Remove method doesn't decrement list count by one.");
         }
 
@@ -54,28 +57,26 @@
         [ExpectedException(typeof(ArgumentException))]
         public void RemovingInvalidItemShouldThrowException()
         {
-            var dynamicList = new DynamicList<char>();
-            dynamicList.Add('a');
-            dynamicList.Remove('b');
+            this.dynamicList.Add('a');
+            this.dynamicList.Remove('b');
         }
 
         [TestMethod]
         public void RemovingItemAtValidPositionShouldDecrementListCountByOne()
         {
-            var dynamicList = new DynamicList<char>();
-            dynamicList.Add('a');
-            dynamicList.RemoveAt(0);
+            this.dynamicList.Add('a');
+            this.dynamicList.RemoveAt(0);
 
-            Assert.AreEqual(0, dynamicList.Count, "RemoveAt method doesn't decrement list count.");
+            Assert.AreEqual(
+                0, this.dynamicList.Count, "RemoveAt method doesn't decrement list count.");
         }
 
         [TestMethod]
         public void RemovingItemAtValidPositionShouldReturnCorrectRemovedItem()
         {
-            var dynamicList = new DynamicList<char>();
-            dynamicList.Add('a');
-            var removed = dynamicList[0];
-            dynamicList.RemoveAt(0);
+            this.dynamicList.Add('a');
+            var removed = this.dynamicList[0];
+            this.dynamicList.RemoveAt(0);
 
             Assert.IsTrue(removed == 'a', "RemoveAt method doesn't remove the correct item.");
         }
@@ -84,9 +85,8 @@
         [ExpectedException(typeof(ArgumentOutOfRangeException))]
         public void RemovingItemAtNegativePositionShouldThrowException()
         {
-            var dynamicList = new DynamicList<char>();
-            dynamicList.Add('a');
-            dynamicList.RemoveAt(-1);
+            this.dynamicList.Add('a');
+            this.dynamicList.RemoveAt(-1);
         }
 
         [TestMethod]
@@ -94,19 +94,17 @@
 
         public void RemovingItemAtInvalidPositionShouldThrowException()
         {
-            var dynamicList = new DynamicList<char>();
-            dynamicList.Add('a');
-            dynamicList.RemoveAt(1);
+            this.dynamicList.Add('a');
+            this.dynamicList.RemoveAt(1);
         }
 
         [TestMethod]
         public void GettingItemByValidIndexShouldReturnCorrectItem()
         {
-            var dynamicList = new DynamicList<char>();
-            dynamicList.Add('a');
+            this.dynamicList.Add('a');
 
             Assert.IsTrue(
-                dynamicList[0] == 'a',
+                this.dynamicList[0] == 'a',
                 "Getting item by index method doesn't return the correct item.");
         }
 
@@ -114,32 +112,29 @@
         [ExpectedException(typeof(ArgumentOutOfRangeException))]
         public void GettingItemByNegativeIndexShouldThrowException()
         {
-            var dynamicList = new DynamicList<char>();
-            dynamicList.Add('a');
-            var position = dynamicList[-1];
+            this.dynamicList.Add('a');
+            var position = this.dynamicList[-1];
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentOutOfRangeException))]
         public void GettingItemByInvalidIndexShouldThrowException()
         {
-            var dynamicList = new DynamicList<char>();
-            dynamicList.Add('a');
-            var position = dynamicList[3];
+            this.dynamicList.Add('a');
+            var position = this.dynamicList[3];
         }
 
         [TestMethod]
         public void SettingItemByValidIndexShouldReturnCorrectItem()
         {
-            var dynamicList = new DynamicList<char>();
-            dynamicList.Add('a');
-            dynamicList.Add('b');
-            dynamicList.Add('c');
+            this.dynamicList.Add('a');
+            this.dynamicList.Add('b');
+            this.dynamicList.Add('c');
 
-            dynamicList[2] = 'd';
+            this.dynamicList[2] = 'd';
 
             Assert.IsTrue(
-                dynamicList[2] == 'd',
+                this.dynamicList[2] == 'd',
                 "Setting item by index method doesn't return correct item.");
         }
 
@@ -147,35 +142,32 @@
         [ExpectedException(typeof(ArgumentOutOfRangeException))]
         public void SettingItemByNegativeIndexShouldThrowException()
         {
-            var dynamicList = new DynamicList<char>();
-            dynamicList.Add('a');
-            dynamicList.Add('b');
-            dynamicList.Add('c');
+            this.dynamicList.Add('a');
+            this.dynamicList.Add('b');
+            this.dynamicList.Add('c');
 
-            dynamicList[-1] = 'd';
+            this.dynamicList[-1] = 'd';
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentOutOfRangeException))]
         public void SettingItemByInvalidIndexShouldThrowException()
         {
-            var dynamicList = new DynamicList<char>();
-            dynamicList.Add('a');
-            dynamicList.Add('b');
-            dynamicList.Add('c');
+            this.dynamicList.Add('a');
+            this.dynamicList.Add('b');
+            this.dynamicList.Add('c');
 
-            dynamicList[3] = 'd';
+            this.dynamicList[3] = 'd';
         }
 
         [TestMethod]
         public void CkeckingIndexOfValidItemShouldReturnCorrectIndex()
         {
-            var dynamicList = new DynamicList<char>();
-            dynamicList.Add('a');
-            dynamicList.Add('b');
-            dynamicList.Add('c');
+            this.dynamicList.Add('a');
+            this.dynamicList.Add('b');
+            this.dynamicList.Add('c');
 
-            int index = dynamicList.IndexOf('b');
+            int index = this.dynamicList.IndexOf('b');
 
             Assert.AreEqual(1, index, "IndexOf method doesn't return correct index.");
         }
@@ -183,12 +175,11 @@
         [TestMethod]
         public void CkeckingIndexOfInvalidItemShouldReturnMinusOne()
         {
-            var dynamicList = new DynamicList<char>();
-            dynamicList.Add('a');
-            dynamicList.Add('b');
-            dynamicList.Add('c');
+            this.dynamicList.Add('a');
+            this.dynamicList.Add('b');
+            this.dynamicList.Add('c');
 
-            int index = dynamicList.IndexOf('z');
+            int index = this.dynamicList.IndexOf('z');
 
             Assert.AreEqual(-1, index, "IndexOf method doesn't -1.");
         }
@@ -196,12 +187,11 @@
         [TestMethod]
         public void CheckingIfExcistingItemContainsShouldReturnTrue()
         {
-            var dynamicList = new DynamicList<char>();
-            dynamicList.Add('a');
-            dynamicList.Add('b');
-            dynamicList.Add('c');
+            this.dynamicList.Add('a');
+            this.dynamicList.Add('b');
+            this.dynamicList.Add('c');
 
-            bool isContains = dynamicList.Contains('a');
+            bool isContains = this.dynamicList.Contains('a');
 
             Assert.AreEqual(true, isContains, "Contains method doesn't return true.");
         }
@@ -209,12 +199,11 @@
         [TestMethod]
         public void CheckingIfNonExcistingItemContainsShouldReturnFalse()
         {
-            var dynamicList = new DynamicList<char>();
-            dynamicList.Add('a');
-            dynamicList.Add('b');
-            dynamicList.Add('c');
+            this.dynamicList.Add('a');
+            this.dynamicList.Add('b');
+            this.dynamicList.Add('c');
 
-            bool isContains = dynamicList.Contains('z');
+            bool isContains = this.dynamicList.Contains('z');
 
             Assert.AreEqual(false, isContains, "Contains method doesn't return false.");
         }
